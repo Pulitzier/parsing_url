6b2f8885c912412d0efa48ea48cfad407c157c1c
ybaravy <ybrv2010@gmail.com>
2017-06-29 08:13:34 +0300

    Get page and retrieve information

--- script/parse_url.rb
+++ script/parse_url.rb
@@ -3,19 +3,35 @@ require 'nokogiri'
 require 'csv'
 
 def parse_page(uri, page)
-	url = Curl.get(uri)
+	# url = Curl.get(uri)
+	# url.perform
+
+	# # File.open(page, 'w') do |f|
+	# # 	f.puts url.body_str
+	# # end
+
+	# doc = Nokogiri.parse(url.body_str)
+	# out = doc.xpath("//body//section[@id='center_column']//a[@class='product-name']/@href").to_a
+
+	# CSV.open(page, 'w') do |row|
+	# 	row << [out.join("\n")]
+	# end
+
+	# CSV.foreach(page, 'r') do |row|
+	# 	product = Curl.get(row)
+
+	url = Curl.get('https://www.petsonic.com/hobbit-alf-barritas-redonda-ternera-para-perros.html')
 	url.perform
 
 	# File.open(page, 'w') do |f|
 	# 	f.puts url.body_str
 	# end
 
-
 	doc = Nokogiri.parse(url.body_str)
 	out = doc.xpath("//body//section[@id='center_column']//a[@class='product-name']/@href").to_a
 
 	CSV.open(page, 'w') do |row|
-		row << [out.join("\n")]
+		row << [out.join(", ")]
 	end
 end
 
